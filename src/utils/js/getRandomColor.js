f32c7e8aac5e748a7663bbaaf4a7dad9eb5ba3bf
Iga Ryś <imirys96@gmail.com>
2023-06-14 21:37:39 +0200

    color, change

--- src/utils/js/getRandomColor.js
+++ src/utils/js/getRandomColor.js
@@ -1,15 +1,15 @@
+
 export const getRandomHslColor = () => {
- 
+  
   const getRandomNumber = (min, max) =>
     Math.round(Math.random() * (max - min) + min);
-
+  
   
   const { hue, saturation, lightness } = {
-    hue: getRandomNumber(210, 230),
+    hue: getRandomNumber(200, 210),
     saturation: getRandomNumber(70, 90),
-    lightness: getRandomNumber(50, 85),
+    lightness: getRandomNumber(65, 85),
   };
-  // Return the string with hsl prefix
+  
   return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-};
-	
+  };
